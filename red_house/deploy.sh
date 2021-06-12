f82fac1301e60f668bf4bc42aef83f6688c05d95
qscftyjm1234 <qscftyjm1234@gmail.com>
2021-06-12 17:51:41 +0800

    122

--- red_house/deploy.sh
+++ red_house/deploy.sh
@@ -1,14 +1,29 @@
 #!/usr/bin/env sh
-# 當發生錯誤時終止腳本運行
+
+# 發生錯誤時執行終止指令
 set -e
-# 打包
+
+# 打包編譯
 npm run build
-# 移動至到打包後的dist目錄 
+
+# 移動到打包資料夾下，若你有調整的話打包後的資料夾請務必調整
 cd dist
-git init //因為dist資料夾預設是被ignore的，因此在進入dist資料夾後初始化git
+
+# 部署到自定義網域
+# echo 'www.example.com' > CNAME
+
+git init
 git add -A
 git commit -m 'deploy'
-# 部署到 https://github.com/chou0728/eric-project.git 分支為 gh-pages
-git push -f https://github.com/chou0728/eric-project.git master:gh-pages
-//將dist資料夾中的內容推送至遠端eric-project的gh-pages分支中，並強制無條件將舊有的內容取代成目前的內容（指令 git push -f)
+
+# 部署到 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# 部署到 https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+# 以這個專案來講就要改成這樣以下這樣，下面是走 ssh 模式
+git push -f git@github.com:qscftyjm1234/red_house.git master:gh-pages
+# 除此之外，也可以改走 HTTPS 模式
+# git push -f https://github.com/hsiangfeng/HexfootMusic.git master:gh-pages
+
 cd -
